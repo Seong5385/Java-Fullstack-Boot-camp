54d3d651264c0ef7838c8da4397ed555bd0c7e3a
seongchan <tjdcks5381@naver.com>
2026-01-19 12:01:12 +0900

    JavaBasic

--- BasicJava/baekjoon/advenced/Q1157.java
+++ BasicJava/baekjoon/advenced/Q1157.java
@@ -8,15 +8,23 @@ import java.util.stream.Collectors;
 public class Q1157 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char[] s = br.readLine().toCharArray();
 
-        Map<Character, Long> count = new String(s).chars().mapToObj(c -> (char) c).filter(Character::isLetterOrDigit)
+        Map<Character, Long> count = new String(br.readLine()).chars().mapToObj(c -> (char) c)
+                .filter(Character::isLetterOrDigit)
                 .collect(Collectors.groupingBy(
                         Character::toUpperCase, Collectors.counting()));
 
-        for (Entry<Character, Long> c : count.entrySet()) {
-            System.out.println(c.getKey() + " : " + c.getValue());
+        List<Character> re = count.entrySet().stream()
+                .filter(v -> v.getValue() == count.values().stream().mapToLong(Long::longValue).max().orElse(0))
+                .map(Entry::getKey)
+                .collect(Collectors.toList());
+
+        if (re.size() > 1) {
+            System.out.println("?");
         }
+
+        else
+            System.out.println(re.get(0));
 
     }
 }
