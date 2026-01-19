94fb5a9efc108a1c9f8dd433517f0363b2846197
seongchan <tjdcks5381@naver.com>
2026-01-19 18:21:15 +0900

    JavaBasic

--- BasicJava/baekjoon/advenced/Q1316.java
+++ BasicJava/baekjoon/advenced/Q1316.java
@@ -1,19 +1,20 @@
 package baekjoon.advenced;
 
 import java.io.*;
-import java.util.*;
 
 public class Q1316 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int count = 0;
 
         int n = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < n; i++) {
-            String str = br.readLine();
-            str.chars().mapToObj(c -> (char) c).filter(c -> c.equals(c)).forEach(System.out::println);
-            ;
+            char[] c = br.readLine().toCharArray();
+
+            for (int j = 1; j < c.length; j++) {
+
+            }
         }
     }
+
 }
