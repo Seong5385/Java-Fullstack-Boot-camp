df0d0339ac8fcebc2031c1012cd8575c9b0c061e
Seong5381 <tjdcks5381@naver.com>
2026-01-23 23:28:42 +0900

    JavaBasic

--- BasicJava/advencedjava/src/collections/Quiz/Lotto.java
+++ BasicJava/advencedjava/src/collections/Quiz/Lotto.java
@@ -1,9 +1,17 @@
 package collections.quiz;
 
-import java.util.Random;
+import java.util.*;
 
 public class Lotto {
     public static void main(String[] args) {
-        new Random().ints(1, 46).distinct().limit(6).forEach(x -> System.out.print(x + " "));
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for(int i=0; i<n; i++) {
+            new Random().ints(1, 46).distinct().limit(6).forEach(x -> System.out.print(x + " "));
+            System.out.println();
+        }
+        
+        in.close();
     }
 }
