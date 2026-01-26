b94301ddd104d24255f035df26b14c5d3f895b10
Seong5381 <tjdcks5381@naver.com>
2026-01-27 01:22:09 +0900

    JavaBasic

--- BasicJava/baekjoon/MapSet/Q1620.java
+++ BasicJava/baekjoon/MapSet/Q1620.java
@@ -12,11 +12,26 @@ public class Q1620 {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        for(int i=0; i<n; i++) {
+        poke.add(0, null);
+
+        for(int i=1; i<=n; i++) {
             poke.add(br.readLine());
         }
 
-        
+        StringBuilder sb= new StringBuilder();
+
+        for(int i=0; i<m; i++) {
+            String s = br.readLine();
+            if(s.chars().allMatch(Character::isDigit)) {
+                sb.append(poke.get(Integer.parseInt(s))).append("\n");
+            }
+
+            else {
+                sb.append(poke.indexOf(s)).append("\n");
+            }
+        }
+
+        System.out.println(sb);
 
         
     }
